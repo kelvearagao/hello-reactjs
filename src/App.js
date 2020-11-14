8b4602455aef5438f257491471325a482c0552f5
Kelve Aragão <kelvearagao@gmail.com>
2020-11-14 17:59:35 -0300

    Permite deploy do site reactjs no github pages

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello ReactJs</p>
+        <p>Hello ReactJs em produção!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
